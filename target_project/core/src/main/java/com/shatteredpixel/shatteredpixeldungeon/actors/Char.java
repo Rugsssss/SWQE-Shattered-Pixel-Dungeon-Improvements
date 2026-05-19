0d3ecee9b914a42bf98fa40b89e68ba1980908b4
amoghrgowda <amoghrgowda09@gmail.com>
2026-05-19 19:56:13 +1000

    fixed a dumb syntax error

--- target_project/core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/Char.java
+++ target_project/core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/Char.java
@@ -597,7 +597,7 @@ public abstract class Char extends Actor {
 			if (combinedLethality != null && this instanceof Hero && ((Hero) this).belongings.attackingWeapon() instanceof MeleeWeapon && combinedLethality.weapon != ((Hero) this).belongings.attackingWeapon()){
 				if ( enemy.isAlive() && enemy.alignment != alignment && !Char.hasProp(enemy, Property.BOSS)
 						&& !Char.hasProp(enemy, Property.MINIBOSS) &&
-						(enemy.HP/(float)enemy.HT) <= COMBINED_LETHALITY_HP_THRESHOLD*(Hero)this).pointsInTalent(Talent.COMBINED_LETHALITY)/3f) {
+						(enemy.HP/(float)enemy.HT) <= COMBINED_LETHALITY_HP_THRESHOLD*((Hero) this).pointsInTalent(Talent.COMBINED_LETHALITY)/3f) {
 					enemy.HP = 0;
 					if (enemy.buff(Brute.BruteRage.class) != null){
 						enemy.buff(Brute.BruteRage.class).detach();
